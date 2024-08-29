7fe7eb59b96a9fb5e694fb4a3f77c339aea483b4
JuggernautTej <tiwatej@gmail.com>
2024-08-29 18:53:15 +0100

    edited task1 again 4

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,19 +14,25 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class"""
+    """ Redacting Formatter class for filtering PII in logs"""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: list):
-        """Initializes the class"""
+        """Initializes the class
+        Args:
+            fields: A list of fields."""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """This method filters the incoming log records using filter_datum"""
+        """This method filters the incoming log records using filter_datum
+        Args:
+            record (logging.LogRecord): The log record to be formatted.
+        Returns:
+            A string of the formated log message with redaction implemented"""
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super().format(record)
