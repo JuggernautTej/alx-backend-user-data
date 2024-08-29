f2e0d6cec574c418e766253eccddeb030eb1eafb
JuggernautTej <tiwatej@gmail.com>
2024-08-29 18:23:54 +0100

    edited task1 again 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,7 +10,7 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """A function that returns the log message obfuscated."""
     patn = "({})=.*?{}".format('|'.join(fields), separator)
-    return re.sub(patn, r"\1={}".format(redaction) + separator + " ", message)
+    return re.sub(patn, r"\1={}".format(redaction) + separator, message)
 
 
 class RedactingFormatter(logging.Formatter):
@@ -27,6 +27,6 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         """This method filters the incoming log records using filter_datum"""
-        source = super(RedactingFormatter, self).format(record)
+        source = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             source, self.SEPARATOR)
