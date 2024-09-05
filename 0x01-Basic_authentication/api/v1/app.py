15b22e55820daef3e232ad95e40d7229767a0e09
JuggernautTej <tiwatej@gmail.com>
2024-09-05 13:56:47 +0100

    rewrote before_request method in app.py file 2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -50,7 +50,7 @@ def before_request():
         return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized', '/api/v1/forbidden']
-    if not auth.require_auth(request.path, excluded_paths):
+    if auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None:
             abort(401, description='Unauthorzed')
         if auth.current_user(request) is None:
