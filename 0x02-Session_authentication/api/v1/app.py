226f2cfc30cb29f93fef45e55eb84c89da5eddd4
JuggernautTej <tiwatej@gmail.com>
2024-09-06 01:21:36 +0100

    updated task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,7 +57,7 @@ def before_request():
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized', '/api/v1/forbidden',
                       '/api/v1/auth_session/login/']
-    request.current_user = auth.current_user(request)
+
     if auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None:
             abort(401, description='Unauthorzed')
@@ -65,6 +65,7 @@ def before_request():
             abort(401, description='Unauthorized')
         if auth.current_user(request) is None:
             abort(403, description='Forbidden')
+        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
