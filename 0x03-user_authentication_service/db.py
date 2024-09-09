0d1d37891d929d7315a3fe6d425616f75e3f56c5
JuggernautTej <tiwatej@gmail.com>
2024-09-09 19:32:28 +0100

    added task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -31,9 +33,40 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a new user to the database and returns the User object."""
-        session = self._session
+        """Adds a new user to the database and returns the User object.
+
+        Args:
+            email (str): The email of the user.
+            hashed_password (str): The hashed password of the user.
+
+        Returns:
+            User: The created User object.
+        """
         new_user = User(email=email, hashed_password=hashed_password)
-        session.add(new_user)
-        session.commit()
+        self._session.add(new_user)
+        self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user by the given keyword arguments.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments to filter the users.
+
+        Returns:
+            User: The first User object that matches the filter criteria.
+
+        Raises:
+            NoResultFound: If no user is found.
+            InvalidRequestError: If invalid query arguments are passed.
+        """
+        session = self._session
+        try:
+            query = session.query(User).filter_by(**kwargs)
+            user = query.first()
+            if user is None:
+                raise NoResultFound("No user found")
+            return user
+        except InvalidRequestError:
+            self._session.rollback()
+            raise
