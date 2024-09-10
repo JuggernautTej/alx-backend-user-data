168f0ab4483d36d34ef973b6c748695b4deff8a8
JuggernautTej <tiwatej@gmail.com>
2024-09-10 13:11:32 +0100

    added task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """The Flask app"""
 
-from flask import Flask, jsonify, Response
+from auth import Auth
+from flask import Flask, jsonify, Response, request, abort
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
@@ -14,6 +16,26 @@ def welcome() -> Response:
         JSON: the payload."""
     p_load = {"message": "Bienvenue"}
     return jsonify(p_load)
+
+
+@app.route("/user", methods=['POST'], strict_slashes=False)
+def users():
+    """Registers a new user"""
+    # Get the email and password from form data
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # Check for validity of provided email and password
+    if not email or not password:
+        abort(400, description="Missing email or password")
+
+    try:
+        # Attempt to register the new user
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        # If user exists, return error message
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
